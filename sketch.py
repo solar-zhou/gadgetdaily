3db0f4b03369a90fc0d3303a62f319dc7bc56e81
solar_zhou <shule_zhou@163.com>
2019-12-02 22:52:52 +0800

    sketch the beauty

---
+++ sketch.py
@@ -0,0 +1,24 @@
+#!usr/bin/env python3
+# _*_ coding: utf-8 _*_
+"""
+@Author: Joshua
+@File: sketch.py
+@Time: 2019/12/2
+"""
+
+import cv2
+
+img_source = 'e:/github/test_imgs/beauty.jpg'
+img_destination = 'beauty.jpg'
+# read source image
+rgb_img = cv2.imread(img_source)
+# rgb to gray
+gray_img = cv2.cvtColor(rgb_img, cv2.COLOR_RGB2GRAY)
+# blur the gray
+gray_img_blurred = cv2.medianBlur(gray_img, 5)
+# Gaussian blur the gray
+img_Gaussian_blurred = cv2.GaussianBlur(gray_img_blurred, ksize=(21, 21), sigmaX=0, sigmaY=0)
+# merge the gray and the blurred gray
+img_edge = cv2.divide(gray_img, img_Gaussian_blurred, scale=255)
+# write the image to the img_destination
+cv2.imwrite(img_destination, img_edge)
